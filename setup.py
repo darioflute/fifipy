cd2ce28cccde8735d54721ea4e9b8d793eb77ca1
Dario Fadda <darioflute@gmail.com>
2018-07-16 16:18:32 -0700

    first files

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""Setup script fo installing the fifipy library."""
+
+from setuptools import setup
+
+config = {
+    'name': 'fifipy',
+    'version': '0.01',
+    'description': 'FIFI-LS Python library',
+    'long_description': 'Collection of programs to reduce FIFI-LS data',
+    'author': 'Dario Fadda',
+    'author_email': 'darioflute@gmail.com',
+    'url': 'https://github.com/darioflute/fifipy.git',
+    'download_url': 'https://github.com/darioflute/fifipy',
+    'license': 'GPLv3+',
+    'packages': ['fifipy'],
+    'scripts': [],
+    'include_package_data': True,
+    'package_data': {'fifipy': ['icons/*.png', 'icons/*.gif', 'stylesheet.css', 'data/CalibrationResults.csv']},
+    'install_requires': ['numpy', 'matplotlib']
+}
+
+setup(**config)
