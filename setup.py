505baec27d2c8a348091e9f6cf4f3ae82acd37a8
Dario Fadda <darioflute@gmail.com>
2023-04-14 11:46:14 -0700

    added new version

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@
 """Setup script fo installing the fifipy library."""
 
 from distutils.core import setup
+import json
+
+with open('fifipy/version.json') as fp:
+    _info = json.load(fp)
 
 config = {
     'name': 'fifipy',
-    'version': '0.4',
+    'version': _info['version'],
     'description': 'FIFI-LS Python library',
     'long_description': 'Collection of programs to reduce FIFI-LS data',
-    'author': 'Dario Fadda',
+    'author': _info['author'],
     'author_email': 'darioflute@gmail.com',
     'url': 'https://github.com/darioflute/fifipy.git',
     'download_url': 'https://github.com/darioflute/fifipy',
@@ -18,7 +22,7 @@ config = {
     'include_package_data': True,
     'package_data': {'fifipy': ['icons/*.png', 'icons/*.gif', 'stylesheet.css',
                                 'data/CalibrationResults.csv','data/*.gz','data/*txt','data/*.fits']},
-    'install_requires': ['numpy', 'matplotlib', 'astropy']
+    'install_requires': ['numpy', 'matplotlib', 'astropy', 'pyqt']
 }
 
 setup(**config)
